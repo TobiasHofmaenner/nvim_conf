0335ca4a894aed0a27ce99f14a236a49526e7f03
TobiasHofmaenner <tobias.hofmaenner@ferrovac.com>
2026-03-26 12:24:10 +0100

    added lsp servers for some languages

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -50,6 +50,30 @@ return {
         capabilities = capabilities,
       })
 
+      vim.lsp.config("clangd", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("pyright", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("rust_analyzer", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("gopls", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("zls", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("intelephense", {
+        capabilities = capabilities,
+      })
+
       require("mason").setup()
       require("mason-lspconfig").setup({
         ensure_installed = {
@@ -59,6 +83,12 @@ return {
           "jsonls",
           "dockerls",
           "docker_compose_language_service",
+          "clangd",
+          "pyright",
+          "rust_analyzer",
+          "gopls",
+          "zls",
+          "intelephense",
         },
         automatic_enable = true,
       })
@@ -66,16 +96,22 @@ return {
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(event)
           local map = function(keys, func, desc, mode)
-            mode = mode or "n"
-            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+            vim.keymap.set(mode or "n", keys, func, {
+              buffer = event.buf,
+              desc = "LSP: " .. desc,
+            })
           end
 
           map("gd", vim.lsp.buf.definition, "Goto Definition")
           map("gr", vim.lsp.buf.references, "Goto References")
           map("gI", vim.lsp.buf.implementation, "Goto Implementation")
+          map("K", vim.lsp.buf.hover, "Hover Documentation")
           map("<leader>rn", vim.lsp.buf.rename, "Rename")
           map("<leader>ca", vim.lsp.buf.code_action, "Code Action", { "n", "x" })
-          map("K", vim.lsp.buf.hover, "Hover Documentation")
+
+          map("<leader>e", vim.diagnostic.open_float, "Line Diagnostics")
+          map("[d", vim.diagnostic.goto_prev, "Previous Diagnostic")
+          map("]d", vim.diagnostic.goto_next, "Next Diagnostic")
         end,
       })
     end,
